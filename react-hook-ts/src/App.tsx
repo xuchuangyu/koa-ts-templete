51aa650e6bdba1d6bba61ba207455c54784aaf1c
xuchuangyu <254568214@qq.com>
2023-12-29 11:34:32 +0800

    first commit

---
+++ react-hook-ts/src/App.tsx
@@ -0,0 +1,15 @@
+/*
+ * @Date: 2023-12-28 10:29:04
+ * @Description:
+ * @LastEditTime: 2023-12-28 15:25:35
+ * @FilePath: \my-app3\src\App.tsx
+ */
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
